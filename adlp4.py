b038cdd7950ec6b477d6f86b4fcd61bfc34f81f2
dongpl <dongpl@spreadst.com>
2018-09-13 11:19:25 +0800

    可以根据dailybuild编译成功后发送任务

--- adlp4.py
+++ adlp4.py
@@ -11,9 +11,12 @@ import json
 from requests.exceptions import ConnectionError
 import traceback
 import multiprocessing
+import subprocess
 
+COUNT_EX = 0
 WHITE_URL = 'http://10.0.70.54:8080/tjxt/index/get_white_list_pac_info'
-STORAGE_FILE = 'urls_infos'
+STORAGE_FILE = 'urls_infos.json'
+DOWLOAD_LAVA = ''
 
 def get_urls():
     r = requests.get(WHITE_URL)
@@ -36,7 +39,6 @@ def get_verify_urlid(urls):
         urls[url]['id'] = bs.select('#main-panel h1')[0].get_text().split('#')[-1]
     return urls
 
-
 def get_verify_id(url):
     r = requests.get(url)
     if r.status_code != 200:
@@ -45,6 +47,7 @@ def get_verify_id(url):
     return bs.select('#main-panel h1')[0].get_text().split('#')[-1]
 
 def download_or_not(urls):
+    global COUNT_EX
     with open(STORAGE_FILE, 'r+') as f:
         urls = json.load(f)
         verifyids = []
@@ -53,6 +56,8 @@ def download_or_not(urls):
                 verifyid = get_verify_id(url)
             except ConnectionError:
                 traceback.print_exc()
+                COUNT_EX += 1
+                print COUNT_EX
                 time.sleep(1)
                 continue
             if verifyid == urls[url]['id']:
@@ -67,38 +72,39 @@ def download_or_not(urls):
         json.dump(urls, f)
     return verifyids
 
-
 def download_pac(url, verifyid):
-    r = requests.get(url, stream=True)
+    r = requests.get(url.replace('lastBuild', verifyid), stream=True)
     if r.status_code != 200:
-        print 'get url fail: %d'%r.status_code
+        print 'get url fail: %d %s'%(r.status_code, url)
         return 'get url fail: %d'%r.status_code
     url_s = url.split('/')
     if 'view' in url:
         branch = url_s[7]
-        project = url_s[12]
-        pac = url_s[-1]
     else:
         branch = url_s[5]
-        project = url_s[9]
-        pac = url_s[-1]
 
-    if not os.path.exists(branch):
-        os.mkdir(branch)
-        os.mkdir(os.path.join(branch, project))
-        os.mkdir(os.path.join(branch, project, verifyid))
+    project_i = url_s.index('Images') + 1
+    project = url_s[project_i]
+    pac = url_s[-1]
+
+    if not os.path.exists(DOWLOAD_LAVA+branch):
+        os.mkdir(DOWLOAD_LAVA+branch)
+        os.mkdir(os.path.join(DOWLOAD_LAVA, branch, project))
+        os.mkdir(os.path.join(DOWLOAD_LAVA, branch, project, verifyid))
     else:
-        if not os.path.exists(os.path.join(branch, project)):
-            os.mkdir(os.path.join(branch, project))
-            os.mkdir(os.path.join(branch, project, verifyid))
-        elif not os.path.exists(os.path.join(branch, project, verifyid)):
-            os.mkdir(os.path.join(branch, project, verifyid))
-    with open(os.path.join(branch, project, verifyid, pac), 'w') as f:
+        if not os.path.exists(os.path.join(DOWLOAD_LAVA, branch, project)):
+            os.mkdir(os.path.join(DOWLOAD_LAVA, branch, project))
+            os.mkdir(os.path.join(DOWLOAD_LAVA, branch, project, verifyid))
+        elif not os.path.exists(os.path.join(DOWLOAD_LAVA, branch, project, verifyid)):
+            os.mkdir(os.path.join(DOWLOAD_LAVA, branch, project, verifyid))
+    dl_p = os.path.join(DOWLOAD_LAVA, branch, project, verifyid, pac)
+    with open(dl_p, 'w') as f:
         for line in r.iter_lines():
             f.write(line)
 
+    request_squrd(dl_p, url, verifyid)
 
-def init_json():
+def init():
     urls = get_urls()
 
     url_id = get_verify_urlid(urls)
@@ -106,16 +112,69 @@ def init_json():
     with open('urls_infos.json', 'r+') as f:
         json.dump(url_id, f)
         f.flush()
+    return url_id
+
+def request_squrd(download_path, daily_url, dailyid):
+    try:
+        dtl = requests.get('http://10.0.70.128:8000/submitinfos/infos/list_pac_urls_j/').json()
+        print dtl
+        device_type = dtl.get(daily_url)
+        if not device_type:
+            print 'Not in white list!', daily_url
+            return
+        dp = 'http://worker05:8080/'+download_path
+        url_s = daily_url.split('/')
+        if 'view' in daily_url:
+            branch = url_s[7]
+        else:
+            branch = url_s[5]
+
+        project_i = url_s.index('Images') + 1
+        project = url_s[project_i]
+
+        b_num = branch+"_"+project+str(dailyid)
+        y_template = os.path.join('template', device_type+'.yaml')
+        cmd = """python submit_for_testing.py \
+              --device-type {LAVA_DEVICE_TYPE} \
+              --build-number  {build_number}\
+              --lava-server {LAVA_SERVER} \
+              --qa-server {QA_SERVER} \
+              --qa-server-team lkft \
+              --qa-server-project {QA_SERVER_PROJECT} \
+              --test-plan {yaml} \
+              --img-path {DOWNLOAD_URL} \
+              --change-id {changeid} \
+              --vts-url {VTS_URL} \
+              --submitter {SUBMIT_USER_NAME} \
+              --qa-token 13194653496cba5209fd1187e37adb5c0260f904"""\
+            .format(LAVA_DEVICE_TYPE=device_type,
+                    build_number=b_num,
+                    LAVA_SERVER='sprd_lava_01',
+                    QA_SERVER='http://10.0.70.105:8000',
+                    QA_SERVER_PROJECT='remote-lab-demo',
+                    yaml=y_template,
+                    DOWNLOAD_URL=dp,
+                    changeid='0',
+                    VTS_URL='0',
+                    SUBMIT_USER_NAME='pac_dloader',
+                    )
+
+        print cmd
+        os.system(cmd)
+    except:
+        traceback.print_exc()
+
 
 if __name__ == '__main__':
 
-    urls = get_urls()
-
-    url_id = get_verify_urlid(urls)
-    print url_id
-    with open(STORAGE_FILE, 'r+') as f:
-        tof = json.dump(url_id, f)
-        f.flush()
+    url_id = init()
+    # urls = get_urls()
+    #
+    # url_id = get_verify_urlid(urls)
+    # print url_id
+    # with open(STORAGE_FILE, 'r+') as f:
+    #     tof = json.dump(url_id, f)
+    #     f.flush()
     process = []
     while True:
         verifyids = download_or_not(url_id)
@@ -126,8 +185,10 @@ if __name__ == '__main__':
             print p.pid
         print process
         for pro in process:
-            pro.join()
-        time.sleep(300)
+            if not pro.is_alive():
+                process.remove(pro)
+        print 'sleep'
+        time.sleep(30)
     # url = 'http://10.0.1.163:8080/jenkins/job/sprdroid4.4_sfphone_17f_rls1/lastBuild/artifact/Images/sp9820e_2h10_native-userdebug-native/sp9820e_2h10_native-userdebug-native_MOCER5_2H10_UNCACHE_SIGNE.pac.gz'
     # verifyid = '214'
     # download_pac(url, verifyid)
---
+++ submit_for_testing.py
@@ -0,0 +1,147 @@
+import argparse
+import os
+import requests
+import sys
+import StringIO
+from copy import deepcopy
+from string import Template
+from ruamel.yaml import YAML
+import traceback
+
+try:
+    from urllib.parse import urlsplit
+except ImportError:
+    from urlparse import urlsplit
+
+
+def _submit_to_squad(lava_job, lava_url_base, qa_server_api, qa_server_base, qa_token, quiet):
+    headers = {
+        "Auth-Token": qa_token
+    }
+    print ("#"*50+qa_token)
+    try:
+        data = {
+            "definition": lava_job,
+            "backend": urlsplit(lava_url_base).netloc  # qa-reports backends are named as lava instances
+        }
+        print("Submit to: %s" % qa_server_api)
+        results = requests.post(qa_server_api, data=data, headers=headers,
+                                timeout=31)
+        if results.status_code < 300:
+            print("%s/testjob/%s" % (qa_server_base, results.text))
+        else:
+            print(results.status_code)
+            print(results.text)
+    except requests.exceptions.RequestException as err:
+        traceback.format_exc()
+        print("QA Reports submission failed")
+        if not quiet:
+            print("offending job definition:")
+            print(lava_job)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device-type",
+                        help="Device type in LAVA",
+                        dest="device_type",
+                        required=True)
+    parser.add_argument("--env-prefix",
+                        help="Prefix for the environment name",
+                        dest="env_prefix",
+                        default="")
+    parser.add_argument("--env-suffix",
+                        help="Suffix for the environment name",
+                        dest="env_suffix",
+                        default="")
+    parser.add_argument("--build-number",
+                        help="Build number",
+                        dest="build_number",
+                        required=True)
+    parser.add_argument("--qa-server-team",
+                        help="Team in QA Reports service",
+                        dest="qa_server_team",
+                        required=True)
+    parser.add_argument("--qa-server-project",
+                        help="Project in QA Reports service",
+                        dest="qa_server_project",
+                        required=True)
+    parser.add_argument("--qa-server",
+                        help="QA Reports server",
+                        dest="qa_server",
+                        default="https://qa-reports.linaro.org")
+    parser.add_argument("--qa-token",
+                        help="QA Reports token",
+                        dest="qa_token",
+                        default=os.environ.get('QA_REPORTS_TOKEN'))
+    parser.add_argument("--lava-server",
+                        help="LAVA server URL",
+                        dest="lava_server",
+                        required=True)
+    parser.add_argument("--test-plan",
+                        help="LAVA test plan.",
+                        dest="test_plan",
+                        required=True)
+    parser.add_argument("--quiet",
+                        help="Only output the final qa-reports URL",
+                        action='store_true',
+                        dest="quiet")
+    parser.add_argument("--img-path",
+                        help="Which img lava to download",
+                        dest="img_path")
+    parser.add_argument("--change-id",
+                        help="Which img lava to download",
+                        dest="changeid")
+    parser.add_argument("--vts-url",
+                        help="vts url",
+                        dest="vts_url")
+    parser.add_argument("--submitter",
+                        help="who submit this test to squad",
+                        dest="submitter")
+
+    args, _ = parser.parse_known_args()
+
+    qa_server_base = args.qa_server
+    if not (qa_server_base.startswith("http://") or qa_server_base.startswith("https://")):
+        qa_server_base = "https://" + qa_server_base
+    qa_server_team = args.qa_server_team
+    qa_server_project = args.qa_server_project
+    qa_server_build = args.build_number
+    vts_url = args.vts_url
+    submitter = args.submitter
+    qa_server_env = args.env_prefix + args.device_type + args.env_suffix
+    qa_server_api = "%s/api/submitjob/%s/%s/%s/%s" % (
+        qa_server_base,
+        qa_server_team,
+        qa_server_project,
+        qa_server_build,
+        qa_server_env)
+    lava_server = args.lava_server
+    changeid = args.changeid
+    img_path = args.img_path
+    if not (lava_server.startswith("http://") or lava_server.startswith("https://")):
+        lava_server = "https://" + lava_server
+    lava_url_base = "%s://%s/" % (urlsplit(lava_server).scheme, urlsplit(lava_server).netloc)
+
+    output = StringIO.StringIO()
+    with open(args.test_plan, 'r') as f:
+        yaml = YAML()
+        values = yaml.load(f)
+        values['actions'][2]['deploy']['images']['pac']['url'] = img_path
+        # values['metadata']['git changeid'] = changeid
+        # values['actions'][5]['test']['definitions'][0]['params']['TEST_URL'] = vts_url
+        # values['metadata']['submitter'] = submitter
+
+        yaml.dump(values, output)
+        lava_job = output.getvalue()
+        print(lava_job)
+
+    _submit_to_squad(lava_job,
+        lava_url_base,
+        qa_server_api,
+        qa_server_base,
+        args.qa_token,
+        args.quiet)
+
+if __name__ == "__main__":
+    main()
